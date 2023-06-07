b280c9cd4ab348cb574ea96de646899f83122c24
Al33ks <96834553+Al33ks@users.noreply.github.com>
2023-06-07 17:26:55 +0200

    Revert "hoping to work"
    This reverts commit 2cbdddc91dafae9ea6be51f2df49103e908f3ddb.

--- app/front-end/src/pages/App/App.tsx
+++ app/front-end/src/pages/App/App.tsx
@@ -2,6 +2,7 @@ import React from "react";
 import "../login/index.css";
 import ReactRoutes from "../routes/routes";
 import { AuthProvider } from "react-auth-kit";
+import { BrowserRouter } from "react-router-dom";
 
 function App() {
   return (
@@ -10,7 +11,7 @@ function App() {
         authType={"cookie"}
         authName={"_auth"}
         cookieDomain={window.location.hostname}
-        cookieSecure={window.location.protocol === "http:"}
+        cookieSecure={window.location.protocol === "https:"}
       >
         <ReactRoutes />
       </AuthProvider>
