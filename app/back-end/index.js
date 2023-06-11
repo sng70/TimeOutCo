4bc9df48165438eb31f094c4161ccfb947801f1b
Al33ks <96834553+Al33ks@users.noreply.github.com>
2023-06-11 17:40:25 +0200

    s

--- app/back-end/index.js
+++ app/back-end/index.js
@@ -50,19 +50,6 @@ app.post("/login", (req, res) => {
             if (roleSecrets.hasOwnProperty(role)) {
               const roleCode = roleSecrets[role];
 
-              // Ustawienie ciasteczka z nazwą i rolą użytkownika
-              if (req.body.remember === "true") {
-                res.cookie("name", name, { maxAge: 30 * 24 * 60 * 60 * 1000 }); // 30 dni
-                res.cookie("role", roleCode, {
-                  maxAge: 30 * 24 * 60 * 60 * 1000,
-                }); // 30 dni
-              } else {
-                // Usunięcie ciasteczek
-                res.clearCookie("name");
-                res.clearCookie("role");
-              }
-
-              // Przekierowanie do strony głównej
               res.redirect(
                 "http://localhost:3000/home?name=" + name + "&role=" + roleCode
               );
